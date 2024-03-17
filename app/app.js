0400b1b80b1788570614034b3016bc885845f502
Melonica <melonica.jasmitha13@gmail.com>
2024-03-17 19:57:11 +0000

    pug setup and login ui

--- app/app.js
+++ app/app.js
@@ -9,37 +9,16 @@ app.use(express.static("static"));
 
 // Get the functions in the db.js file to use
 const db = require('./services/db');
+app.set('view engine', 'pug');
+app.set('views', './app/views');
 
-// Create a route for root - /
-app.get("/", function(req, res) {
-    res.send("Hello world!");
+
+app.get('/', function (req, res) {
+    res.render('login');
 });
 
-// Create a route for testing the db
-app.get("/db_test", function(req, res) {
-    // Assumes a table called test_table exists in your database
-    sql = 'select * from test_table';
-    db.query(sql).then(results => {
-        console.log(results);
-        res.send(results)
-    });
-});
-
-// Create a route for /goodbye
-// Responds to a 'GET' request
-app.get("/goodbye", function(req, res) {
-    res.send("Goodbye world!");
-});
-
-// Create a dynamic route for /hello/<name>, where name is any value provided by user
-// At the end of the URL
-// Responds to a 'GET' request
-app.get("/hello/:name", function(req, res) {
-    // req.params contains any parameters in the request
-    // We can examine it in the console for debugging purposes
-    console.log(req.params);
-    //  Retrieve the 'name' parameter and use it in a dynamically generated page
-    res.send("Hello " + req.params.name);
+app.get('/login', function (req, res) {
+    res.send('hello');
 });
 
 // Start server on port 3000
