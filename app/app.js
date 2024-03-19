5ddf1fd7b6b38d140d1a367a0c8bc3e11bcb47f3
InduReddy <koshirei@roehampton.ac.uk>
2024-03-19 01:14:18 +0000

    registration and registration api

--- app/app.js
+++ app/app.js
@@ -1,9 +1,14 @@
 // Import express.js
 const express = require("express");
+const bodyParser = require('body-parser');
 
 // Create express app
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Parse JSON bodies (as sent by API clients)
+app.use(bodyParser.json());
 // Add static files location
 app.use(express.static("static"));
 
@@ -14,12 +19,33 @@ app.set('views', './app/views');
 
 
 app.get('/', function (req, res) {
+    
     res.render('login');
 });
 
 app.get('/login', function (req, res) {
     res.send('hello');
 });
+app.get('/register', function (req, res) {
+    res.render('register');
+});
+
+// Registration route
+app.post('/register-page', async function (req, res) {
+    console.log(req.body)
+    const { username, email, password, fullname, dob, gender } = req.body;
+    const sql = 'INSERT INTO Users (username, email, password, fullname, dob, gender) VALUES (?, ?, ?, ?, ?, ?)';
+    const values = [username, email, password, fullname, dob, gender];
+    try {
+        await db.query(sql, values);
+        res.status(200).send('Registration successful');
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
 
 // Start server on port 3000
 app.listen(3000,function(){
